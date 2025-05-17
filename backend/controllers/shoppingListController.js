05bfe530c17a46286e4762dc2088232b1ef2ad17
Hongyue Zhang <hongyue.zhang@student.lut.fi>
2025-05-18 04:49:28 +0800

    a

--- backend/controllers/shoppingListController.js
+++ backend/controllers/shoppingListController.js
@@ -26,35 +26,6 @@ const createList = asyncHandler(async (req, res) => {
   res.status(201).json(list);
 });
 
-// @desc    Update goal
-// @route   PUT /api/goals/:id
-// @access  Private
-const updateGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id)
-
-  if (!goal) {
-    res.status(400)
-    throw new Error('Goal not found')
-  }
-
-  // Check for user
-  if (!req.user) {
-    res.status(401)
-    throw new Error('User not found')
-  }
-
-  // Make sure the logged in user matches the goal user
-  if (goal.user.toString() !== req.user.id) {
-    res.status(401)
-    throw new Error('User not authorized')
-  }
-
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  })
-
-  res.status(200).json(updatedGoal)
-})
 
 // @desc    Delete list
 // @route   DELETE /api/lists/:id
--- readme.md
+++ readme.md
@@ -1,1 +1,24 @@
 # my-porject
+
+You can add your MONGO_URI
+Install dependencies
+# Backend deps
+npm install
+
+# Frontend deps
+cd frontend
+npm install
+
+npm install concurrently --save-dev
+npm run dev
+
+If it is not work:
+1) Open a new terminal
+    cd \backend
+    npm install
+    npm run server
+    If it shows Server started on port 5000, then the backend is OK
+2) Open another terminal
+    cd \frontend
+    npm start
+    Open the browser and visit http://localhost:3000. The /api proxy will then be correctly forwarded to 5000.
